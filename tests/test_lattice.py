a37e158e1330c7b4ed3bb0e4b6c61f96309f2285
Perry <yrrepy@gmail.com>
2026-09-13 21:16:55 -0700

    Add support for hexagonal lattices

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -60,6 +60,26 @@ _RECT_PLANES = """\
 205  pz  2.0
 206  pz -2.0"""
 
+SQRT3 = 1.7320508076
+
+# Manual Listing 10.8: hexagon with an apothem of 1 cm centered at the origin
+_HEX_PLANES = """\
+301  px  1.0
+302  px -1.0
+303  p   1.0  1.7320508076  0.0  2.0
+304  p  -1.0  1.7320508076  0.0  2.0
+305  p   1.0  1.7320508076  0.0 -2.0
+306  p  -1.0  1.7320508076  0.0 -2.0"""
+
+# Same hexagon with R along y
+_HEX_PLANES_Y = """\
+311  py  1.0
+312  py -1.0
+313  p  -1.7320508076  1.0  0.0  2.0
+314  p  -1.7320508076  1.0  0.0 -2.0
+315  p  -1.7320508076 -1.0  0.0  2.0
+316  p  -1.7320508076 -1.0  0.0 -2.0"""
+
 
 def _fill_card(indices, univ_ids, per_line=9):
     """Build the FILL card of a lattice cell as continuation lines."""
@@ -113,5 +133,28 @@ def test_rect_lattice_3d():
     ranges = ((-1, 1), (-1, 1), (-1, 1))
     vectors = ((0., 0., -4.), (2., 0., 0.), (0., 3., 0.))
     model = _convert(1, '206 -205 -201 202 -203 204', _RECT_PLANES,
+                     '-1:1 -1:1 -1:1', _FILL_3D)
+    _assert_elements(model, ranges, (0., 0., 0.), vectors, _FILL_3D)
+
+
+@mark.parametrize("surfaces,region,center,vectors", [
+    param(_HEX_PLANES, '-301 302 -303 305 -304 306', (0., 0., 0.),
+          ((2., 0., 0.), (1., SQRT3, 0.), (0., 0., 0.)), id='planes'),
+    param('1  rhp  2 0.5 -5   0 0 10   1 0 0', '-1', (2., 0.5, 0.),
+          ((2., 0., 0.), (1., SQRT3, 0.), (0., 0., 10.)), id='rhp-off-center'),
+    param(_HEX_PLANES_Y, '-311 312 -313 314 -315 316', (0., 0., 0.),
+          ((0., 2., 0.), (-SQRT3, 1., 0.), (0., 0., 0.)), id='r-along-y'),
+])
+def test_hex_lattice(surfaces, region, center, vectors):
+    ranges = ((-2, 2), (-2, 2), (0, 0))
+    model = _convert(2, region, surfaces, '-2:2 -2:2 0:0', _FILL_2D)
+    _assert_elements(model, ranges, center, vectors, _FILL_2D)
+
+
+def test_hex_lattice_3d():
+    # Height vector of the prism pointing along -z
+    ranges = ((-1, 1), (-1, 1), (-1, 1))
+    vectors = ((2., 0., 0.), (1., -SQRT3, 0.), (0., 0., -10.))
+    model = _convert(2, '-1', '1  rhp  0 0 5   0 0 -10   1 0 0',
                      '-1:1 -1:1 -1:1', _FILL_3D)
     _assert_elements(model, ranges, (0., 0., 0.), vectors, _FILL_3D)
